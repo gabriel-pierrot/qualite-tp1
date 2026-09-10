8839f13bdf66cbccb34f2933271e27b2606f7a57
unknown <gabriel.pierrot@crie.i-univ-tlse2.fr>
2026-09-10 10:04:17 +0200

    modification

--- src/main/java/iut/Main.java
+++ src/main/java/iut/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Calculatrice");
-        System.out.println("Fonction Soustraction opérationnelle dans la calculatrice");
+        System.out.println("Fonction Addition opérationnelle dans la calculatrice");
     }
 }
