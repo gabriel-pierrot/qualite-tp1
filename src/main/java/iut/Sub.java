b938d546d147a8b3cc0804014285cda72239e34b
Alexis Sudre Brito <alexis.sudrebrito@etu.univ-tlse2.fr>
2026-09-10 08:56:43 +0200

    Ajout de la classe Sub

---
+++ src/main/java/iut/Sub.java
@@ -0,0 +1,13 @@
+package iut;
+
+/*
+@author Alexis Sudre Brito
+ */
+public class Sub {
+    public static int sub(int a, int b){
+        int resultat;
+        resultat = a-b;
+
+        return resultat;
+    }
+}
