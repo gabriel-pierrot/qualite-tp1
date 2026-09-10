a77ae79ff75e24dea99a7eb7171730079585c9cf
unknown <gabriel.pierrot@crie.i-univ-tlse2.fr>
2026-09-10 09:05:56 +0200

    Ajout de AddTest

---
+++ src/test/java/iut/AddTest.java
@@ -0,0 +1,8 @@
+public class AddTest {
+    public static void addTest() {
+        int a = 5;
+        int b = 3;
+        int resultat = add(a, b);
+        assertEquals(8, resultat, "5 + 3 est égal à 8";
+    }
+}
